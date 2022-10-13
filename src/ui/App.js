c82b785e75e3107a80b2ac5fbdfbe0a413659231
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-10-13 10:45:45 +0900

    Remove all routes and only `EditorContianer`

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,26 +1,15 @@
 import React, { Component, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
-import configs from "../configs";
 
 import GlobalStyle from "./GlobalStyle";
 
 import Loading from "./Loading";
-import Error from "./Error";
 
 import { ApiContextProvider } from "./contexts/ApiContext";
 import { AuthContextProvider } from "./contexts/AuthContext";
 
-import RedirectRoute from "./router/RedirectRoute";
 import { Telemetry } from "../telemetry";
-
-import LandingPage from "./landing/LandingPage";
-import WhatsNewPage from "./whats-new/WhatsNewPage";
-import LoginPage from "./auth/LoginPage";
-import LogoutPage from "./auth/LogoutPage";
-import ProjectsPage from "./projects/ProjectsPage";
-import CreateProjectPage from "./projects/CreateProjectPage";
-import CreateScenePage from "./projects/CreateScenePage";
 
 import { ThemeProvider } from "styled-components";
 
@@ -30,10 +19,6 @@ import theme from "./theme";
 
 const EditorContainer = lazy(() =>
   import(/* webpackChunkName: "project-page", webpackPrefetch: true */ "./EditorContainer")
-);
-
-const PackageKitPage = lazy(() =>
-  import(/* webpackChunkName: "package-kit-page", webpackPrefetch: true */ "./assets/PackageKitPage")
 );
 
 export default class App extends Component {
@@ -72,19 +57,7 @@ export default class App extends Component {
               <GlobalStyle />
               <Column as={Suspense} fallback={<Loading message="Loading..." fullScreen />}>
                 <Switch>
-                  {configs.isMoz() && <Route path="/" exact component={LandingPage} />}
-                  {!configs.isMoz() && <RedirectRoute path="/" exact to="/projects" />}
-                  <Route path="/whats-new" exact component={WhatsNewPage} />
-                  <RedirectRoute path="/new" exact to="/projects" />
-                  <Route path="/login" exact component={LoginPage} />
-                  <Route path="/logout" exact component={LogoutPage} />
-                  <Route path="/projects/create" exact component={CreateProjectPage} />
-                  <RedirectRoute path="/projects/templates" exact to="/projects/create" />
-                  <Route path="/projects" exact component={ProjectsPage} />
-                  <Route path="/projects/:projectId" component={EditorContainer} />
-                  <Route path="/kits/package" component={PackageKitPage} />
-                  <Route path="/scenes/:sceneId" component={CreateScenePage} />
-                  <Route render={() => <Error message="Page not found." />} />
+                  <Route path="*" component={EditorContainer} />
                 </Switch>
               </Column>
               <Telemetry />
